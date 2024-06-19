bf8115635e50faafd712d49114331e932758f3fc
DJ Preet <33035404+PREETCHAUHAN@users.noreply.github.com>
2024-06-19 08:51:43 +0530

    Update tgmusicbot.py

--- tgmusicbot.py
+++ tgmusicbot.py
@@ -25,14 +25,13 @@ Send a link as a reply to bypass Music category check
 # requirements.txt
 OpenCC
 Pillow
-youtube-dl
+yt_dlp
 
 # ../../config.py
 MUSIC_CHATS = [
-    -1234567891012,
-    -2345678910123
+    -1001451526459
 ]
-MUSIC_USERS = [1234567890]
+MUSIC_USERS = [1852829593]
 MUSIC_DELAY_DELETE_INFORM = 10
 MUSIC_INFORM_AVAILABILITY = (
     "This bot only serves the specified group and"
